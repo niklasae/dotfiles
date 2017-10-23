46829ed9d653bd110c8e62c8773fa7ebfa10cc63
Niklas A Emanuelsson <n.emanuelsson@gmail.com>
2017-10-23 16:06:36 +0200

    Tmux: Script to start jbl-web dev env

---
+++ tmux/dev_jbl-web.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+session_name=`basename "$0" .sh`  # https://stackoverflow.com/a/7820227/537598
+path="~/Code/work/jbl-web"
+# Start session with one code window split between source/vagrant/ipython
+tmux new-session -d -s "$session_name" -n "code" "cd $path && vim README.md"
+tmux split-window -v -p 20 "cd $path && vagrant up && vagrant ssh"
+tmux split-window -h -p 50 # FIXME: "cd $path && ipython"
+# Add new window for python libs
+tmux new-window -n "libs" "cd ~/.virtualenvs/jbl-web/lib/python2.7/site-packages && vim django/__init__.py +NERDTreeFind"
+# Select first window
+tmux select-window -t 1
+tmux -2 attach-session -t "$session_name"
