ed2a2c22114cba148e7a8e200a361d7eab033968
Niklas A Emanuelsson <n.emanuelsson@gmail.com>
2017-10-26 12:40:46 +0200

    tmux - Add TODOs to env script

--- tmux/dev_jbl-web.sh
+++ tmux/dev_jbl-web.sh
@@ -6,8 +6,10 @@ path="~/Code/work/jbl-web"
 tmux new-session -d -s "$session_name" -n "code" "cd $path && vim README.md"
 tmux split-window -v -p 20 "cd $path && vagrant up && vagrant ssh"
 tmux split-window -h -p 50 # FIXME: "cd $path && ipython"
-# Add new window for python libs
+# Window for python libs
 tmux new-window -n "libs" "cd ~/.virtualenvs/jbl-web/lib/python2.7/site-packages && vim django/__init__.py +NERDTreeFind"
+# Window for todo-list
+tmux new-window -n "todos" "cd ~/Development/todos && vim jobylon.today.TODO -c 'vsplit jobylon.week.TODO' -c 'vsplit jobylon.year.TODO'"
 # Select first window
 tmux select-window -t 1
 tmux -2 attach-session -t "$session_name"
